63cd2526e738599669cce62a318c4486dc3b1b4e
sankar <sankusanku001@gmail.com>
2023-03-11 09:22:01 +0530

    nutritionasit module completed

--- nutritionaist.py
+++ nutritionaist.py
@@ -6,3 +6,101 @@ nutritionaist=Blueprint('nutritionaist',__name__)
 @nutritionaist.route('/nutritionaisthome')
 def nutritionaisthome():
     return render_template('nutritionaisthome.html')
+
+
+@nutritionaist.route('/nutritionaist_view_profile',methods=['get','post'])
+def nutritionaist_view_profile():
+    data={}
+
+    data={}
+    q="select * from  nutretion where nutretion_id='%s'"%(session['nid'])
+    data['res']=select(q)
+    data['count']=len(select(q))
+
+    if 'action' in request.args:
+        action=request.args['action']
+    else:
+        action=None
+
+    
+    if action == "update":
+        q="select * from  nutretion where nutretion_id='%s'"%(session['nid'])
+        val=select(q)
+        data['raw']=val
+
+        if 'update' in request.form:
+            fname=request.form['fname']
+            lname=request.form['lname']
+            place=request.form['place']
+            phone=request.form['phone']
+            email=request.form['email']
+
+            q="update nutretion set fname='%s',lname='%s',place='%s', phone='%s', email='%s' where nutretion_id='%s' "%(fname,lname,place,phone,email,session['nid'])
+            update(q)
+            flash("Updated Successfully")
+            return redirect(url_for("nutritionaist.nutritionaist_view_profile"))
+
+    return render_template('nutritionaist_view_profile.html',data=data) 
+
+@nutritionaist.route('/nutritionaist_view_players')
+def nutritionaist_view_players():
+    data={}
+    q="select * from player"
+    data['res']=select(q)   
+    return render_template('nutritionaist_view_players.html',data=data)
+
+
+@nutritionaist.route('/nutritionaist_view_coach')
+def nutritionaist_view_coach():
+    data={}
+    q="select * from coach"
+    data['res']=select(q)   
+    return render_template('nutritionaist_view_coach.html',data=data)
+
+@nutritionaist.route('/nutritionaist_view_news')
+def nutritionaist_view_news():
+    data={}
+    q="select * from news"
+    data['res']=select(q)   
+    return render_template('nutritionaist_view_news.html',data=data)
+
+
+@nutritionaist.route('/nutritionaist_view_rank')
+def nutritionaist_view_rank():
+    data={}
+    q="select * from rank inner join player using (player_id)"
+    data['res']=select(q)   
+    return render_template('nutritionaist_view_rank.html',data=data)
+
+
+@nutritionaist.route('/nutritionaist_complaints',methods=['get','post'])
+def nutritionaist_complaints():
+    data={}
+    if 'btn' in request.form:
+        comp=request.form['comp']
+        q="insert into complaint values (null,'%s','pending',curdate(),'%s','nutritionaist')"%(comp,session['loginid'])
+        insert(q)
+        flash("Complaint Added!")
+        return redirect(url_for('nutritionaist.nutritionaist_complaints'))
+    
+    q="select * from complaint where sender_id='%s'"%(session['loginid'])
+    data['res']=select(q)   
+    return render_template('nutritionaist_complaints.html',data=data)
+
+
+@nutritionaist.route('/nutritionaist_consultation',methods=['get','post'])
+def nutritionaist_consultation():
+    data={}
+    q="select * from psysician"
+    data['ps']=select(q)
+    if 'btn' in request.form:
+        reason=request.form['reason']
+        pid=request.form['pid']
+        q="insert into consultation values (null,'%s','%s','%s','pending')"%(pid,session['loginid'],reason)
+        insert(q)
+        flash("You will receive your reply soon")
+        return redirect(url_for('nutritionaist.nutritionaist_consultation'))
+    
+    q="select * from consultation inner join psysician using (psysician_id) where sender_id='%s'"%(session['loginid'])
+    data['res']=select(q)   
+    return render_template('nutritionaist_consultation.html',data=data)
--- public.py
+++ public.py
@@ -32,6 +32,13 @@ def login():
                     session['eid']=val1[0]['enquiry_id']
                     flash("Login Success")
                     return redirect(url_for("enquiry.enquiryhome"))
+            elif utype == "nutritionaist":
+                q="select * from nutretion where login_id='%s'"%(session['loginid'])
+                val7=select(q)
+                if val7:
+                    session['nid']=val7[0]['nutretion_id']
+                    flash("Login Success")
+                    return redirect(url_for("nutritionaist.nutritionaisthome"))
                
             
             else:
@@ -68,3 +75,30 @@ def enquiry_reg():
             flash("Registration successfull")
             return redirect(url_for("public.login"))
     return render_template("enquiry_reg.html")
+
+
+@public.route("/nutritionaist_reg",methods=['get','post'])
+def nutritionaist_reg():
+    if 'btn' in request.form:
+        fname=request.form['fname']
+        lname=request.form['lname']
+        phone=request.form['phone']
+        email=request.form['email']
+        place=request.form['place']
+       
+        pwd=request.form['pwd']
+        uname=request.form['uname']
+      
+
+        q="select * from login where username='%s'"%(uname)
+        res=select(q)
+        if res:
+            flash("This Username already exist!, try register with new one.")
+        else:
+            q="insert into login values(null,'%s','%s','pending')"%(uname,pwd)
+            lid=insert(q)
+            q="insert into nutretion values (NULL,'%s','%s','%s','%s','%s','%s')"%(lid,fname,lname,place,phone,email)
+            insert(q)
+            flash("Registration successfull")
+            return redirect(url_for("public.login"))
+    return render_template("nutritionaist_reg.html")
